f45354c3a1cb4e01601ce42edc91c72e42444235
Vasile Boris <boris@espressoprogrammer.com>
2019-01-14 08:57:41 +0200

    Add skills template

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ function render(resume) {
 		partialsDir = path.join(__dirname, 'partials'),
 		filenames = fs.readdirSync(partialsDir);
 
-	Handlebars.registerHelper('and', (...params) => params.length ? params.reduce((acc, p) => acc && p) : false);
-	Handlebars.registerHelper('or', (...params) => params.length ? params.reduce((acc, p) => acc || p) : false);
-	Handlebars.registerHelper('buildAddress', buildAddress);
+	Handlebars.registerHelper('and', and);
+	Handlebars.registerHelper('or', or);
 	Handlebars.registerHelper('buildInstitution', buildInstitution);
 	Handlebars.registerHelper('buildPosition', buildPosition);
     Handlebars.registerHelper('buildAchievement', buildAchievement);
+    Handlebars.registerHelper('concatArray', concatArray);
+	Handlebars.registerHelper('concatFields', concatFields);
 
 	filenames.forEach(function (filename) {
 	  const matches = /^([^.]+).hbs$/.exec(filename);
@@ -31,16 +32,33 @@ function render(resume) {
 	});
 }
 
-const buildAddress = location => {
-	const details = ['address', 'postalCode', 'city', 'region', 'countryCode'];
-	return details.reduce((acc, detail) => location[detail] ? `${acc}${location[detail]} ` : acc, '')
+const and = (...params) => {
+	const length = params.length - 1;
+	return length > 0 ? params.slice(0, length).reduce((acc, p) => acc && p) : false;
 };
 
-const buildInstitution = (institution, website) => {
+const or = (...params) => {
+	const length = params.length - 1;
+	return length > 0 ? params.slice(0, length).reduce((acc, p) => acc || p) : false;
+};
+
+const concatFields = (object, ...fields) => {
+    const values = fields.slice(0, fields.length - 1).map(field => object[field]);
+    return concatArray(values);
+};
+
+const concatArray = values => values.filter(value => value !== null && value !== undefined).join(", ");
+
+const buildInstitution = (context, institutionField, websiteField) => {
+	const institution = context[institutionField],
+		website = context[websiteField];
 	return website ? `[${institution}](${website})` : institution;
 };
 
-const buildPosition = (position, startDate, endDate) => {
+const buildPosition = (context, positionField, startDateField, endDateField) => {
+	const position = context[positionField],
+		startDate = context[startDateField],
+		endDate = context[endDateField];
 	let result = '';
 	if(position) {
 		result += `**${position}**`;
@@ -52,7 +70,9 @@ const buildPosition = (position, startDate, endDate) => {
 	return result;
 };
 
-const buildAchievement = (achievement, date) => {
+const buildAchievement = (context, achievementField, dateField) => {
+	const achievement = context[achievementField],
+		date = context[dateField];
     let result = '';
     if(achievement) {
         result += `**${achievement}**`;
